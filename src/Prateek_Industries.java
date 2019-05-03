92051b939a811bb4d9796a05a24575b3dee1385f
PrateekSingh29 <48443151+PrateekSingh29@users.noreply.github.com>
2019-05-03 20:25:52 +0530

    Add files via upload

---
+++ src/Prateek_Industries.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package prateek_industries;
+
+/**
+ *
+ * @author family
+ */
+public class Prateek_Industries {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("printing");
+     Home h = new Home();
+     h.setVisible(true);
+     // TODO code application logic here
+    }
+}
